58e0908ff3354e56379766790d1b53f0679110a1
AlexShows <tashows@gmail.com>
2014-08-13 21:08:11 -0500

    Adding function to merge plaintext into image data Not finished yet, but started putting this together. Need to test the masking in a separate sandbox and get this logic right, then update the rest of the function.

--- tsStego/tsStego.cpp
+++ tsStego/tsStego.cpp
@@ -94,6 +94,33 @@ void write_png_to_file(const char* filename, std::vector<unsigned char>& image, 
 	}
 }
 
+// Take the 8 bits per char and split them 3-2-3, putting 3 bits into the Red, 2 bits into the Green,
+// 3 bits into the Blue, and nothing in Alpha
+// The bits will be placed starting from the LSB of each byte so as to make the least impact to the 
+// image when viewed by a human
+// We put fewer bits into the Green channel because human eyes are more sensitive to a yellowish-green
+void merge_plaintext_into_img_data(std::vector<unsigned char>& plaintext, std::vector<unsigned char>& img_data)
+{
+	// Bounds check
+	// Using 4 * the plaintext size because each element in img_data is a color channel of a pixel, of which
+	// there are 4 channels per pixel, and we're going to overwrite certain bits across three of the channels
+	if (img_data.size() < 4 * plaintext.size())
+		throw std::exception("Exception in merge_plaintext_into_img_data: image is too small to fit all the text");
+
+	// An iterator for moving through the image data
+	auto img_ptr = img_data.begin();
+	unsigned char tmp = 0;
+
+	// Loop through all the characters
+	for (auto& c : plaintext)
+	{
+		// First 3 MSBs from the char are put into the Red channels 3 LSBs
+		tmp = c & 0xE0; // mask off just the 3 MSBs
+		tmp = tmp >> 5; // shift those 3 bits to the right 5 to make them align with the 3 LSBs
+		c = c & tmp; // 
+	}
+}
+
 // TODO: 
 //		- Load the PNG file into the image data structure [ DONE ] 
 //		- Load the plain text file [ DONE ] 
